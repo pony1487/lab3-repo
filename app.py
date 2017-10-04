c5fb935799b9803413f56674ad59a0f1d7a867cd
pony1487 <connollyro@gmail.com>
2017-10-04 12:49:52 +0000

    Fixed insert,added Update

--- app.py
+++ app.py
@@ -17,13 +17,29 @@ def hello():
     cur.execute('''SELECT * FROM students''')
     rv = cur.fetchall()
     return str(rv)
-    cur.close
+    cur.close()
+    
 @app.route("/add")
 def add():
     cur = mysql.connection.cursor() 
-    cur.execute("""INSERT INTO students(studentName,email)VALUES(%s,%s);""",("John Smith","email@"))
+    cur.execute("""INSERT INTO students(studentName,email)VALUES(%s,%s)""",("John Smith","email@"))
+    cur.execute('''SELECT * FROM students''')
     ab = cur.fetchall()
     return str(ab)
-    cur.close
+    cur.close()
+
+@app.route("/update")
+def update():
+    cur = mysql.connection.cursor()
+
+    querey = "Update students SET studentName = %s WHERE studentID =%s"
+    cur.execute(querey,("BOOOBBBBB",1))
+    cur.execute('''SELECT * from students''')
+    ab = cur.fetchall()
+    
+    return "Update" + str(ab)
+    
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port='5000')
